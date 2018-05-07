e08c0f5a96f7bfc7714a88eb69c70371b8c09201
Arto Rantanen <arto.rantanen@helsinki.fi>
2018-05-07 19:25:19 +0300

    Week 1 exercises

--- W1.hs
+++ W1.hs
@@ -9,24 +9,32 @@ module W1 where
 -- Ex 1: define variables one, two and three. They should all have
 -- type Int and values 1, 2 and 3. This exercise has no tests.
 
+one :: Integer
+one = 1
+two :: Integer
+two = 2
+three :: Integer
+three = 3
+
 -- Ex 2: define the function double of type Integer->Integer. Double
 -- should take one argument and return it multiplied by two.
 
 double :: Integer -> Integer
-double x = undefined
+double x = two * x 
 
 -- Ex 3: define the function quadruple that uses the function double
 -- from the previous exercise to return its argument multiplied by
 -- four.
 
 quadruple :: Integer -> Integer
-quadruple x = undefined
+quadruple x = double $ double x
 
 -- Ex 4: define the function poly2. It should take four arguments of
 -- type Double, a, b, c, and x and return a*x^2+b*x+c. Give poly2 a
 -- type signature, i.e. poly2 :: something.
 
-poly2 = undefined
+poly2 :: Double -> Double  -> Double -> Double -> Double
+poly2 a b c x = a * x^two + b * x + c
 
 -- Ex 5: define the function eeny that returns "eeny" for even inputs
 -- and "meeny" for odd inputs.
@@ -34,7 +42,7 @@ poly2 = undefined
 -- Ps. have a look at the built in function "even"
 
 eeny :: Integer -> String
-eeny = undefined
+eeny x = if even x then "eeny" else "meeny"
 
 -- Ex 6: fizzbuzz! Define the a function fizzbuzz that returns "Fizz"
 -- for numbers divisible by 3, "Buzz" for numbers divisible by 5, and
@@ -42,9 +50,15 @@ eeny = undefined
 -- returns the empty string.
 --
 -- You can use the function mod to compute modulo.
+isDivisible :: Integer -> Integer -> Bool
+isDivisible n k =  (mod n k) == 0
 
-fizzbuzz = undefined
-
+fizzbuzz :: Integer -> String
+fizzbuzz x 
+    | (isDivisible x 5)  && (isDivisible x three) = "FizzBuzz"  
+    | (isDivisible x 5) = "Buzz" 
+    | (isDivisible x three) = "Fizz" 
+    | otherwise = ""
 -- Ex 7: define a function isZero that returns True if it is given an
 -- Integer that is 0, and False otherwise. Give isZero a type signature.
 --
@@ -52,20 +66,24 @@ fizzbuzz = undefined
 --
 -- Ps. the type of booleans in haskell is Bool
 
-isZero = undefined
+isZero :: Integer -> Bool
+isZero 0 = True
+isZero _ = False
 
 -- Ex 8: implement using recursion a function sumTo such that
 --   sumTo n
 -- computes the sum 1+2+...+n
 
 sumTo :: Integer -> Integer
-sumTo = undefined
+sumTo 0 = 0
+sumTo n = n + sumTo(n-1)
 
 -- Ex 9: power n k should compute n to the power k (i.e. n^k)
 -- Use recursion.
 
 power :: Integer -> Integer -> Integer
-power = undefined
+power x 0 = one
+power x y = x * power x (y-1)
 
 -- Ex 10: ilog2 n should be the number of times you can halve the
 -- integer n (rounding down) before you get 1.
@@ -74,7 +92,11 @@ power = undefined
 -- division.
 
 ilog2 :: Integer -> Integer
-ilog2 = undefined
+ilog2 x = ilog2' 0 x
+
+ilog2' :: Integer -> Integer -> Integer
+ilog2' n 1 = n
+ilog2' n x = ilog2'(n+1) (div x 2)
 
 -- Ex 11: compute binomial coefficients using recursion. Binomial
 -- coefficients are defined by the following equations:
@@ -86,7 +108,9 @@ ilog2 = undefined
 -- Hint! pattern matching is your friend.
 
 binomial :: Integer -> Integer -> Integer
-binomial = undefined
+binomial n 0 = one
+binomial 0 k = if k > 0 then 0 else (binomial (-1) k) + (binomial (-1) (k-1))
+binomial n k = (binomial (n-1) k) + (binomial (n-1) (k-1))
 
 -- Ex 12: The tribonacci numbers are defined by the equations
 --
@@ -99,13 +123,18 @@ binomial = undefined
 -- computes T(n). You'll probably want to define a helper function.
 
 tribonacci :: Integer -> Integer
-tribonacci = undefined
+tribonacci 1 = one
+tribonacci 2 = one
+tribonacci 3 = two
+tribonacci n = tribonacci' two one one n
+    where tribonacci' a b c n = if n < 4 then a else tribonacci' (a+b+c) a b  (n-1)
 
 -- Ex 13: implement the euclidean algorithm for finding the greatest
 -- common divisor: http://en.wikipedia.org/wiki/Euclidean_algorithm
 
 myGcd :: Integer -> Integer -> Integer
-myGcd = undefined
+myGcd a 0 = a
+myGcd a b = myGcd b (mod a b)   
 
 -- Ex 14: The Haskell Prelude (standard library) defines the type
 -- Ordering with values LT, GT and EQ. You try out Ordering by
@@ -133,7 +162,11 @@ myGcd = undefined
 --   funnyCompare 2 3 ==> LT
 
 funnyCompare :: Int -> Int -> Ordering
-funnyCompare = undefined
+funnyCompare a b
+    | (even a && even b) || (odd a && odd b) = compare a b
+    | even a && odd b = LT
+    | odd a && even b = GT
+    | otherwise = LT    
 
 -- Ex 15: Implement the function funnyMin that returns the minimum of
 -- its two arguments, according to the ordering implemented by
@@ -144,8 +177,7 @@ funnyCompare = undefined
 -- expression or define a helper function.
 
 funnyMin :: Int -> Int -> Int
-funnyMin = undefined
-
+funnyMin a b = if funnyCompare a b == LT then a else b 
 -- Ex 16: implement the recursive function pyramid that returns
 -- strings like this:
 --
@@ -160,7 +192,10 @@ funnyMin = undefined
 -- * you'll need a (recursive) helper function
 
 pyramid :: Integer -> String
-pyramid = undefined
+pyramid 0 = "0"
+pyramid n = (pyramid' n one 0) ++ "," ++ (pyramid' 0 (-1) (n-1))
+    where pyramid' limit factor n = if n == limit then (show n) else (show n) ++ "," ++ (pyramid' limit factor (n+factor))
+
 
 -- Ex 17: implement the function smallestDivisor that returns the
 -- smallest number (greater than 1) that divides the given number.
@@ -175,7 +210,8 @@ pyramid = undefined
 -- remember this in the next exercise!
 
 smallestDivisor :: Integer -> Integer
-smallestDivisor = undefined
+smallestDivisor n = smallestDivisor' n two
+    where smallestDivisor' n k = if (isDivisible n k) then k else smallestDivisor' n (k + one)
 
 -- Ex 18: implement a function isPrime that checks if the given number
 -- is a prime number. Use the function smallestDivisor.
@@ -183,11 +219,14 @@ smallestDivisor = undefined
 -- Ps. 0 and 1 are not prime numbers
 
 isPrime :: Integer -> Bool
-isPrime = undefined
+isPrime 0 = False
+isPrime 1 = False
+isPrime n = (smallestDivisor n) == n
 
 -- Ex 19: implement a function nextPrime that returns the first prime
 -- number that comes after the given number. Use the function isPrime
 -- you just defined.
 
 nextPrime :: Integer -> Integer
-nextPrime = undefined
+nextPrime n = if isPrime nn then nn else nextPrime nn
+    where nn = n + one
